530ddbf9e2571d35c304207e528239b5c8e3aa32
stevebiczyk <stevebiczyk@gmail.com>
2024-12-24 08:45:27 +0000

    created basic flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+
+# My App
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ in "__main__":
+    app.run(debug=True)
