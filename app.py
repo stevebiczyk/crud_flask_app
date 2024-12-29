71459571e9a62b714d18047ad8213972bf480fe5
stevebiczyk <stevebiczyk@gmail.com>
2024-12-29 17:25:39 +0000

    sending data to html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 # Models
 class NewTask(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, server_default=datetime.now().isoformat())
     
@@ -22,9 +22,35 @@ class NewTask(db.Model):
         return f'<Task {self.id}>'
 
 # Routes to Web Pages
-@app.route("/")
+# Home Page
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        current_task = request.form['content']
+        new_task = NewTask(content=current_task)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue adding your task"
+# List all current tasks
+    else:
+        tasks = NewTask.query.order_by(NewTask.created_at).all()
+        return render_template("index.html", tasks=tasks)
+
+
+
+# Update a task
+@app.route("/update", methods=['GET', 'POST'])
+def update():
+    return render_template("update.html")
+
+# Delete a task
+@app.route("/delete", methods=['GET', 'POST'])
+def delete():
+    return render_template("delete.html")
 
 
 # Run and Debug
